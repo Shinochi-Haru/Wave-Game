9cb399ef15fff63d947b16568655515e3fc8e289
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-11-22 21:34:15 +0900

    [add] ターンのカウント表示を行うクラスを作成

---
+++ Assets/Maruoka/Class/CurrentTurnPresenter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentTurnPresenter : MonoBehaviour
+{
+    private WaveAnimation _nowTurn = null;
+    [SerializeField]
+    private Text _turnText = null;
+
+    void Start()
+    {
+        _nowTurn = GetComponent<WaveAnimation>();
+        if (_turnText == null)
+        {
+            Debug.LogError("ターン表示用テキストをアサインしてください！");
+        }
+    }
+    public void UpdateValue()
+    {
+        _turnText.text = $"{(_nowTurn.WavesPulledCount / 2) + 1}";
+    }
+}
